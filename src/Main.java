bb1eceb4b8cf36f48faf92b8deefd26562e92974
sevenone711 <arbitronin@yandex.ru>
2020-11-16 13:24:29 +0300

    Chat с закрытием и чисткой

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    final String PATH_TO_XML = "views/sample.fxml";
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("views/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource(PATH_TO_XML));
+        primaryStage.setTitle("ChatFx");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,4 +1,39 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+
+    @FXML
+    private TextField messegTextField;
+    @FXML
+    private ListView<String> listView;
+    ObservableList<String> wordsList = FXCollections.observableArrayList();
+    @FXML
+    private void initialize(){
+    listView.setItems(wordsList);
+    }
+    @FXML
+    public void addWordToList(){
+        String word = messegTextField.getText();
+        if (!word.isBlank()){
+            listView.getItems().add(word);
+        }
+        messegTextField.setText("");
+    }
+    @FXML
+    public void close(){
+        System.exit(0);
+    }
+    @FXML
+    public void cleanChat(){
+        listView.getItems().clear();
+    }
+
+
 }
